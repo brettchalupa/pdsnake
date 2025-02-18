43b1fcaad75319016c5c5a5fa7ae7677e7d9f8c4
Brett Chalupa <brettchalupa@gmail.com>
2025-02-18 15:28:30 -0500

    pdsnake: display ver, build, and author + polish start

--- source/main_menu.lua
+++ source/main_menu.lua
@@ -1,11 +1,37 @@
 local gfx <const> = playdate.graphics
 
+local screenWidth <const> = playdate.display.getWidth()
+local screenHeight <const> = playdate.display.getHeight()
+
+local version <const> = playdate.metadata.version
+local build <const> = playdate.metadata.buildNumber
+local versionAndBuild <const> = version .. " (" .. build .. ")"
+local author <const> = playdate.metadata.author
+
+local xPad <const> = 12
+
+local updates = 0
+
 function updateMainMenu()
+	updates += 1
+
 	if playdate.buttonIsPressed(playdate.kButtonA) then
 		switchScene("gameplay")
 	end
 
 	gfx.clear()
-	gfx.drawText("*PDSnake*", 40, 40);
-	gfx.drawText("Press A to Start", 120, 40);
+
+	gfx.drawText("*PDSnake*", xPad, 40);
+
+	if updates % 60 < 30 then
+		gfx.drawText("A   Start", xPad + 4, 120);
+		gfx.drawCircleAtPoint(
+			xPad + 8,
+			128,
+			12
+		)
+	end
+
+	gfx.drawText(versionAndBuild, 12, screenHeight - 32);
+	gfx.drawText(author, screenWidth - 116, screenHeight - 32);
 end
